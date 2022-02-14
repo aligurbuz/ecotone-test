f4459bf2d871dd04f56e87624bc6b102cf6d38bf
aligurbuz <galiant781@gmail.com>
2022-02-14 11:16:36 +0000

    stock repository fix

--- app/Repositories/Resources/Stocks/StocksRepository.php
+++ app/Repositories/Resources/Stocks/StocksRepository.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
@@ -102,7 +102,8 @@ class StocksRepository extends EloquentRepository implements StocksRepositoryCon
 	 */
 	public function eventFireAfterCreate(array $result = [], array $clientData = []): void
 	{
-		//
+		picqer($result)->endpoint('products/'.$result['idProduct'].'/stock/'.$result['idwarehouse']);
+            //->post();
 	}
 
 
@@ -115,6 +116,7 @@ class StocksRepository extends EloquentRepository implements StocksRepositoryCon
 	 */
 	public function eventFireAfterUpdate(array $result = [], array $clientData = []): void
 	{
-		//
+        picqer($result)->endpoint('products/'.$result['idProduct'].'/stock/'.$result['idwarehouse']);
+        //->update();
 	}
 }
